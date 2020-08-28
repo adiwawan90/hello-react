994787dc4e9d67ef581e0d32a219dec861ba018a
adiwawan90 <adi.wawan90@gmail.com>
2020-08-28 14:26:12 +0700

    Updates

---
+++ precache-manifest.9ab11b35527e05e841a1ddc73a3bb8ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767522282131755b75309a8ce61da58b",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "79c4c28576565de19f5d",
+    "url": "/hello-react/static/css/main.a4719c86.chunk.css"
+  },
+  {
+    "revision": "42ad66e3681fc1f5ac83",
+    "url": "/hello-react/static/js/2.fd8834a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-react/static/js/2.fd8834a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c4c28576565de19f5d",
+    "url": "/hello-react/static/js/main.b1eb7db8.chunk.js"
+  },
+  {
+    "revision": "3953ad2c80a259496302",
+    "url": "/hello-react/static/js/runtime-main.750410c0.js"
+  }
+]);
